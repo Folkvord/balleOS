e29c4dde6d726ca6e6465f52940fd22b7b702ec1
FOLKVORD <folkvord.kristoffer@gmail.com>
2025-04-21 23:54:31 +0200

    enkel timer lib

---
+++ cpu/timer.c
@@ -0,0 +1,34 @@
+#include "../drivers/screen.h"
+#include "../drivers/port.h"
+#include "../util/type.h"
+#include "timer.h"
+#include "isr.h"
+
+u32 tick = 0;
+
+static void timer_callback(registers r){
+
+  tick++;
+  char tick_str[256];
+  itoa(tick, tick_str);
+  kprint(tick_str);
+  kprint(" ");
+
+}
+
+void init_timer(u32 freq){
+
+  // Installerer interrupten
+  register_interrupt_handler(IRQ0, timer_callback);
+
+  // PIT-verdien med en 1193180 hz klokke
+  u32 divisor = 1193180 / freq;
+  u8 lower = (u8)(divisor & 0xff);
+  u8 upper = (u8)((divisor >> 8) & 0xff);
+
+  // Sender kommandoen
+  port_byte_out(0x43, 0x36);
+  port_byte_out(0x40, lower);
+  port_byte_out(0x40, upper);
+
+}
---
+++ cpu/timer.h
@@ -0,0 +1,8 @@
+#ifndef CPU_TIMER
+#define CPU_TIMER
+
+#include "../util/type.h"
+
+void init_timer(u32 freq);
+
+#endif
