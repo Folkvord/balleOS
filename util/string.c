91533a6205b14520773cc93b6d07724966bae130
FOLKVORD <folkvord.kristoffer@gmail.com>
2025-04-28 22:50:16 +0200

    strcpy

--- util/string.c
+++ util/string.c
@@ -1,51 +1,67 @@
 #include "string.h"
 
+#define OK 0
+#define ERROR 1
+
 /* Returnerer lengden på en string, eksluderende nullterminatoren */
-int strlen(char* str){
+int strlen(char str[]){
   int i = 0;
   while(str[i] != '\0') i++;
   return i;
 }
 
-/* Tar en liten bokstav og returnerer den store versjonen av den.
-  Hvis noe annet enn en bokstav sendes, retrurneres det tegnet. */
-char toupper(char c){
-  if(c < 'a' || c > 'z') return c;
-  return (c - 32);
-}
-
-/* Tar en stor bokstav og returnerer den lille versjonen av den
-  Hvis noe annet enn en bokstav sendes, retrurneres det tegnet. */
-char tolower(char c){
-  if(c < 'A' || c > 'Z') return c;
-  return (c + 32);
-}
-
 /* Sammenligner to stringer med hverandre. Om de er like returnerer den 0, hvis ikke: 1 */
-int strcmp(char* str1, char* str2){
+int strcmp(char str1[], char str2[]){
   int i = 0;
   while(1){
-    if(str1[i] != str2[i]) return 1;
-    if(str1[i] == '\0' && str2[i] == '\0') return 0;
+    if(str1[i] != str2[i]) return ERROR;
+    if(str1[i] == '\0' && str2[i] == '\0') return OK;
     i++;
   }
 }
 
+/* Kopierer en string inn til en annen. Returnerer dersom stringen er større enn bufferet */
+int strcpy(char str[], char* buffer){
+  if(strlen(str) >= sizeof(buffer)) return ERROR;
+  int i = 0;
+  while(str[i] != '\0'){
+    buffer[i] = str[i];
+    i++;
+  }
+  str[i] = '\0';
+  return OK;
+}
+
+/* Tar en liten bokstav og returnerer den store versjonen av den.
+  Hvis noe annet enn en bokstav sendes, retrurneres det tegnet. */
+  char toupper(char c){
+    if(c < 'a' || c > 'z') return c;
+    return (c - 32);
+  }
+  
+  /* Tar en stor bokstav og returnerer den lille versjonen av den
+    Hvis noe annet enn en bokstav sendes, retrurneres det tegnet. */
+  char tolower(char c){
+    if(c < 'A' || c > 'Z') return c;
+    return (c + 32);
+  }
+  
+
 /* Legger til et tegn i et buffer */
-void append(char* str, char c){
+void append(char str[], char c){
   int len = strlen(str);
   str[len] = c;
   str[len+1] = '\0';
 }
 
 /* Fjerner det siste tegnet i et buffer */
-void pop(char* str){
+void pop(char str[]){
   int len = strlen(str);
   str[len-1] = '\0';
 }
 
 /* Klargjører et buffer */
-void purge(char* str){
+void purge(char str[]){
   int len = strlen(str), i = 0;
   while(i < len){
     str[i] = '\0';
--- util/string.h
+++ util/string.h
@@ -1,12 +1,13 @@
 #ifndef STRING_H
 #define STRING_H
 
-int strlen(char* str);
+int strlen(char str[]);
+int strcmp(char str1[], char str2[]);
+int strcpy(char str[], char* buffer);
 char toupper(char c);
 char tolower(char c);
-int strcmp(char* str1, char* str2);
-void append(char* buffer, char c);
-void pop(char* buffer);
-void purge(char* buffer);
+void append(char buffer[], char c);
+void pop(char buffer[]);
+void purge(char buffer[]);
 
 #endif
